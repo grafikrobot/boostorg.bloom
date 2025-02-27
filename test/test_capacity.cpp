7683c75c94295eaead1156ace806430a5ab19d37
joaquintides <joaquin@tid.es>
2025-02-27 20:46:36 +0100

    added missing include

--- test/test_capacity.cpp
+++ test/test_capacity.cpp
@@ -6,6 +6,7 @@
  * See https://www.boost.org/libs/bloom for library home page.
  */
 
+#include <boost/core/lightweight_test.hpp>
 #include <boost/mp11/algorithm.hpp>
 #include <new>
 #include "test_types.hpp"
