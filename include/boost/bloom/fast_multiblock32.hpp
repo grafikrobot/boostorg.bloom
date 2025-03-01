2e060b7eb6964848f54253273c9a5f5ad050ba61
joaquintides <joaquin@tid.es>
2025-03-01 16:07:30 +0100

    added comment

--- include/boost/bloom/fast_multiblock32.hpp
+++ include/boost/bloom/fast_multiblock32.hpp
@@ -15,7 +15,7 @@
 
 #if defined(BOOST_BLOOM_AVX2)
 #include <boost/bloom/detail/fast_multiblock32_avx2.hpp>
-#elif defined(BOOST_BLOOM_SSE2)
+#elif defined(BOOST_BLOOM_SSE2) /* important that this comes after AVX2 */
 #include <boost/bloom/detail/fast_multiblock32_sse2.hpp>
 #elif defined(BOOST_BLOOM_LITTLE_ENDIAN_NEON)
 #include <boost/bloom/detail/fast_multiblock32_neon.hpp>
