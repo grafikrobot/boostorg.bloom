c049bc3d5f96bacaffec9b33ab743f34d9985c6b
joaquintides <joaquin@tid.es>
2025-03-04 09:47:57 +0100

    s/used_block_size/bucket_size in unaligned cases

--- include/boost/bloom/detail/core.hpp
+++ include/boost/bloom/detail/core.hpp
@@ -193,7 +193,7 @@ private:
       alignof(block_type)>cacheline?alignof(block_type):cacheline:
       1;
   static constexpr std::size_t prefetched_cachelines=
-    1+(used_block_size+cacheline-1-gcd_pow2(bucket_size,cacheline))/cacheline;
+    1+(bucket_size+cacheline-1-gcd_pow2(bucket_size,cacheline))/cacheline;
   using hash_strategy=detail::mcg_and_fastrange;
 
 public:
@@ -513,7 +513,7 @@ private:
     std::false_type /* blocks not aligned */)const
   {
     block_type x;
-    std::memcpy(&x,p,used_block_size);
+    std::memcpy(&x,p,bucket_size);
     return subfilter::check(x,hash);
   }
 
@@ -534,9 +534,9 @@ private:
     std::false_type /* blocks not aligned */)
   {
     block_type x;
-    std::memcpy(&x,p,used_block_size);
+    std::memcpy(&x,p,bucket_size);
     subfilter::mark(x,hash);
-    std::memcpy(p,&x,used_block_size);
+    std::memcpy(p,&x,bucket_size);
   }
 
   BOOST_FORCEINLINE 
